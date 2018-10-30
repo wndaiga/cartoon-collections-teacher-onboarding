0661d3eaae4dcf91b700904d23bece8988c4a5aa
Wachira Ndaiga <wachira.ndaiga@flatironschool.com>
2018-10-30 16:08:49 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,21 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(dwarves_array)# code an argument here
   # Your code here
+  itemized = dwarves_array.each_with_index.map { |dwarve, index| (index+1).to_s()+". "+dwarve }
+  itemized.each { |dwarve_item| puts dwarve_item }
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(planeteer_array)# code an argument here
   # Your code here
+  planeteer_array.map { |call| call.capitalize()+"!" }
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(planeteer_array)# code an argument here
   # Your code here
+  planeteer_array.select { |call| call.size() > 4 }.size() > 0 ? true : false
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(cheese_array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese_array.select { |cheese| cheese_types.include?(cheese) }.first
 end
